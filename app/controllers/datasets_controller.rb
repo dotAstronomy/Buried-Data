1cb61242db0df4f99bcec6dbe57b5923ed5170d1
Arfon Smith <arfon.smith@gmail.com>
2009-12-02 13:10:50 +0100

    Adding cap configs

--- app/controllers/datasets_controller.rb
+++ app/controllers/datasets_controller.rb
@@ -2,4 +2,20 @@ class DatasetsController < ApplicationController
   def index
     
   end
+  
+  def new
+    
+  end
+  
+  def create
+    
+  end
+  
+  def show
+    
+  end
+  
+  def destroy
+    
+  end
 end
