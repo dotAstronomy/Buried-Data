b88d6786d35d7c894f7db57a60598677a8e20b25
Arfon Smith <arfon.smith@gmail.com>
2009-12-03 15:29:56 +0100

    Typo

--- app/models/attachable.rb
+++ app/models/attachable.rb
@@ -1,9 +1,12 @@
 class Attachable < ActiveRecord::Base
   belongs_to :dataset
   
-  has_attached_file :file
-                      
-  # validates_attachment_presence :file
+  has_attached_file :file,
+                    :storage => :s3,
+                    :s3_credentials => "#{RAILS_ROOT}/config/s3.yml",
+                    :bucket => "buried-data",
+                    :path => "attachables/:filename"
+                    
   validates_presence_of :name
                     
 end
