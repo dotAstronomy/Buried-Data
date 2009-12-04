c86ba66835439bf2592ac6600027b42d0ef640c5
Arfon Smith <arfon.smith@gmail.com>
2009-12-04 17:39:16 +0100

    Freezing factory_girl and shoulda

--- config/environment.rb
+++ config/environment.rb
@@ -21,6 +21,8 @@ Rails::Initializer.run do |config|
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem "authlogic"
   config.gem "paperclip"
+  config.gem "shoulda"
+  config.gem "factory_girl"
   
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
