e0d82e1fa85fa58535f7d67a2337e1882d295606
Carolina Ödman <odman@dhcp19-144.physics.ox.ac.uk>
2010-02-10 13:24:04 +0100

    dding template file that look fancy

---
+++ templates/index.php
@@ -0,0 +1,205 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+<head>
+
+<title>Buried Data</title>
+
+<meta http-equiv="content-type" content="application/xhtml+xml; charset=UTF-8" />
+<meta name="author" content="Erwin Aligam - styleshout.com" />
+<meta name="description" content="Astronomical Graveyoard" />
+<meta name="keywords" content="keywords, here" />
+<meta name="robots" content="index, follow, noarchive" />
+<meta name="googlebot" content="noarchive" />
+<link rel="stylesheet" type="text/css" media="screen" href="burieddata.css" />
+</head>
+
+<body>
+<!-- wrap starts here -->	
+<div id="wrap">
+
+	<div id="header">
+	
+		<h1 id="logo-text">Buried<span class="gray">data</span></h1>			
+		<h2 id="slogan">I've started you finish</h2>	
+				
+		<div id="header-tabs">
+			<ul>
+				<li id="current"><a href="index.html"><span>Home</span></a></li>
+				<li><a href="index.html"><span>About</span></a></li>
+				<li><a href="index.html"><span>Upload</span></a></li>
+				<li><a href="index.html"><span>Download</span></a></li>
+				<li><a href="index.html"><span>Help</span></a></li>
+				<li><a href="index.html"><span>Login</span></a></li>			
+			</ul>
+		</div>				
+		
+	</div>
+	
+
+
+<!-- content-wrap starts here -->
+	  <div id="content-wrap">
+	  
+	  		<div id="main"> 
+	
+				<a name="TemplateInfo"></a>
+
+
+				<h1>Buried Data in h1 header</h1>
+				
+                <p>Many of us have data that - if we're honest - is unlikely to see the light of day. It might have been neglected as we moved from job to job, and from place to place, or we might just have been distracted. Collaborators move on, or even leave astronomy, and their data stays behind. What if there was a place to leave your orphaned raw data, half-reduced data, or half-written papers? BuriedData.org is a halfway house and a refuge for data in need of care and attention. 
+                </p>
+
+				<h2>Buried Data in h2 header</h2>
+
+                <p>
+               BuriedData is also a dating service. We match interesting data with people who need it. Someone else's old observations might provide the key to a problem, or help you with your own research. BuriedData.org might reveal your pet object in a different wavelength, or provide support for a cherished observing proposal. Find research projects for undergraduates. Experts on a particular instrument or data reduction technique will find data ready for them to exploit. By starting not from scratch, but from wherever others have left off, you can turn data into results faster than before.
+                </p>
+				
+
+				<h2>Buried Data in h3 header</h2>
+
+                <p>
+               BuriedData is also a dating service. We match interesting data with people who need it. Someone else's old observations might provide the key to a problem, or help you with your own research. BuriedData.org might reveal your pet object in a different wavelength, or provide support for a cherished observing proposal. Find research projects for undergraduates. Experts on a particular instrument or data reduction technique will find data ready for them to exploit. By starting not from scratch, but from wherever others have left off, you can turn data into results faster than before.
+                </p>
+				<p class="comments">
+					<a href="index.html">Read more</a> |
+					<a href="index.html">Comments(7)</a> |
+					Oct 08, 2006
+				</p>
+			
+				<a name="SampleTags"></a>				
+				<h1>Sample Tags</h1>
+				
+				<h3>Code</h3>				
+				<p><code>
+				code-sample { <br />
+				font-weight: bold;<br />
+				font-style: italic;<br />				
+				}
+				</code></p>	
+				
+				<h3>Example Lists</h3>
+				
+				<ol>
+					<li><span>example of ordered list</span></li>
+					<li><span>uses span to color the numbers</span></li>								
+				</ol>	
+							
+				<ul>
+					<li><span>example of unordered list</span></li>
+					<li><span>uses span to color the bullets</span></li>								
+				</ul>				
+				
+				<h3>Blockquote</h3>			
+				<blockquote><p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy 
+				nibh euismod tincidunt ut laoreet dolore magna aliquam erat....</p></blockquote>
+				
+				<h3>Image and text</h3>
+				<p><a href="http://getfirefox.com/"><img src="images/dragon.jpg" width="100" height="120" alt="firefox" class="float-left" /></a>
+				Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec libero. Suspendisse bibendum. 
+				Cras id urna. Morbi tincidunt, orci ac convallis aliquam, lectus turpis varius lorem, eu 
+				posuere nunc justo tempus leo. Donec mattis, purus nec placerat bibendum, dui pede condimentum 
+				odio, ac blandit ante orci ut diam. Cras fringilla magna. Phasellus suscipit, leo a pharetra 
+				condimentum, lorem tellus eleifend magna, eget fringilla velit magna id neque. Curabitur vel urna. 
+				In tristique orci porttitor ipsum. Aliquam ornare diam iaculis nibh. Proin luctus, velit pulvinar 
+				ullamcorper nonummy, mauris enim eleifend urna, congue egestas elit lectus eu est. 				
+				</p>
+								
+				<h3>Example Form</h3>
+				<form action="#">		
+				<p>				
+				<label>Name</label>
+				<input name="dname" value="Your Name" type="text" size="30" />
+				<label>Email</label>
+				<input name="demail" value="Your Email" type="text" size="30" />
+				<label>Your Comments</label>
+				<textarea rows="5" cols="5"></textarea>
+				<br />	
+				<input class="button" type="submit" />		
+				</p>		
+				</form>				
+				<br />		
+								
+	  		</div> 	
+			  
+	  		<div id="sidebar">
+					
+				<h1>Sidebar Menu</h1>				
+				<ul class="sidemenu">
+					<li><a href="index.html">Home</a></li>
+					<li><a href="#TemplateInfo">Template Info</a></li>
+					<li><a href="#SampleTags">Sample Tags</a></li>
+					<li><a href="http://www.styleshout.com/">More Free Templates</a></li>	
+					<li><a href="http://www.dreamtemplate.com" title="Web Templates">Web Templates</a></li>
+      		    </ul>
+				
+				<h1>Site Partners</h1>				
+				<ul class="sidemenu">
+                    <li><a href="http://www.dreamtemplate.com" title="Website Templates">DreamTemplate</a></li>
+                    <li><a href="http://www.themelayouts.com" title="WordPress Themes">ThemeLayouts</a></li>
+                    <li><a href="http://www.imhosted.com" title="Website Hosting">ImHosted.com</a></li>
+                    <li><a href="http://www.dreamstock.com" title="Stock Photos">DreamStock</a></li>
+                    <li><a href="http://www.evrsoft.com" title="Website Builder">Evrsoft</a></li>
+                    <li><a href="http://www.seostation.com" title="SEO">SEOStation</a></li>
+  				</ul>	
+				
+				<h1>Search</h1>	
+				<form method="post" id="search" action="#">
+					<p>
+					<input name="search_query" class="textbox" type="text" /> 
+					<input name="search" class="searchbutton" value="Search" type="submit" />
+					</p>
+				</form>	
+									
+				<h1>Wise Words</h1>
+				<p>&quot;Criticism is something you can avoid easily by saying nothing,
+				doing nothing, and being nothing&quot;</p>	
+				
+				<p class="align-right">- Aristotle</p>
+				
+				<h1>Support Styleshout</h1>
+				<p>If you are interested in supporting my work and would like to contribute, you are
+				welcome to make a small donation through the 
+				<a href="http://www.styleshout.com/">donate link</a> on my website - it will 
+				be a great help and will surely be appreciated.</p>
+				
+			</div>	
+		
+		<!-- content-wrap ends here -->
+		</div>
+		
+
+				<div id="footer">
+
+					<p id="footer-left">
+						&copy; 2010 BuriedData.org |
+						<a href="http://www.dotastronomy.com/" title="dotAstronomy">A DotAstronomy product</a>
+					</p>
+
+					<p id="footer-right">
+						<a href="index.html">Home</a> |
+		                <a href="index.html">Sitemap</a> |
+		                <a href="index.html">RSS Feed</a> |
+		                <a href="http://validator.w3.org/check?uri=referer">XHTML</a> |
+						<a href="http://jigsaw.w3.org/css-validator/check/referer">CSS</a>
+					</p>
+
+				</div>	
+
+		<!-- wrap ends here -->		
+		</div>	
+
+
+	</body>
+
+
+	<!--
+
+	add any scripts you want e.g. google analytics, etc.
+
+	-->
+
+	</html>
